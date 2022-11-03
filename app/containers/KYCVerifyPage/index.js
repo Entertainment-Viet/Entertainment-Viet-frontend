ef1affb09b37b465ab1581d97276b7520bc76fd0
Hồ Huy Hoàng <48915815+hohuyhoangg@users.noreply.github.com>
2022-11-04 00:37:14 +0700

    update city

--- app/containers/KYCVerifyPage/index.js
+++ app/containers/KYCVerifyPage/index.js
@@ -33,7 +33,11 @@ import reducer from './reducer';
 import { AddAvatarIcon } from '../TalentManagementPage/ProviderIcons';
 import InputCustomV2 from '../../components/Controls/InputCustomV2';
 import SelectCustom from '../../components/Controls/SelectCustom';
-import { bankName, dataDistrictHCM } from '../../utils/data-address';
+import {
+  bankName,
+  dataDistrictHCM,
+  dataProvince,
+} from '../../utils/data-address';
 import { QWERTYEditor } from '../../components/Controls';
 import example from './image/example.png';
 import {
@@ -382,20 +386,28 @@ export function KYCVerifyPage({ talentInfo, loadTalent }) {
                       <CustomFormLabel>
                         {t(messages.province())}
                       </CustomFormLabel>
-                      <InputCustomV2
+                      <SelectCustom
                         id="province"
-                        type="text"
                         size="md"
-                        placeholder="Enter your province"
-                        {...register('province', {
-                          required: 'This is required',
-                          minLength: {
-                            value: 4,
-                            message: 'Minimum length should be 4',
-                          },
-                        })}
-                        defaultValue={talentInfo.address.city}
-                      />
+                        {...register('province')}
+                        value={
+                          dataProvince.filter(
+                            item => item.name === talentInfo.address.city,
+                          )[0].name
+                        }
+                        defaultValue={
+                          dataProvince.filter(
+                            item => item.name === talentInfo.address.city,
+                          )[0].name
+                        }
+                      >
+                        {dataProvince.map((option, index) => (
+                          // eslint-disable-next-line react/no-array-index-key
+                          <option key={index} value={option.name}>
+                            {option.name}
+                          </option>
+                        ))}
+                      </SelectCustom>
                       <Text color={RED_COLOR}>
                         {errors.province && errors.province.message}
                       </Text>
