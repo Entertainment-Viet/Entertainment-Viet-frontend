da400a44a5080e0ccfd5763f004dc11fec9b89ac
TrNgTien <ngoctien27122001@gmail.com>
2022-11-11 20:56:26 +0700

    Rebase from develop to FE-37

--- app/containers/PreCheckout/index.js
+++ app/containers/PreCheckout/index.js
@@ -34,7 +34,6 @@ export function PreCheckout({ onLoadData, cartData }) {
   useEffect(() => {
     onLoadData();
   }, []);
-
   const { t } = useTranslation();
   const { content } = cartData;
   const sortedPackage =
