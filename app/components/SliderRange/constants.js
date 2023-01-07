5e09b8c1ea8850d0df94da3b417c00ac8f5f05c6
Bảo Đặng Kim <dangkimbao1999@gmail.com>
2023-01-08 00:40:05 +0700

    fix: fix logic in event result and result page

--- app/components/SliderRange/constants.js
+++ app/components/SliderRange/constants.js
@@ -1,7 +1,7 @@
 export const CHANGE_PRICE_RANGE = 'App/SearchResultPage/CHANGE_PRICE_RANGE';
 export const CHANGE_EVENT_PRICE_RANGE =
   'App/EventSearchResultPage/CHANGE_EVENT_PRICE_RANGE';
-export const MAX_DEFAULT = 100000000;
+export const MAX_DEFAULT = 10000000;
 export const MIN_DEFAULT = 100000;
 export const CHANGE_OWN_PACKAGE_PRICE_RANGE =
   'App/MyPackage/CHANGE_PACKAGE_PRICE_RANGE';
