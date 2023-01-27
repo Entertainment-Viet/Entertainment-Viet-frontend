866c7d27919ca07d9ece8ee382a324ab4a36d7d3
Bảo Đặng Kim <dangkimbao1999@gmail.com>
2023-01-28 02:04:45 +0700

    feat: update responsive for event detail

--- app/containers/EventDetailPage/NormalProfile.js
+++ app/containers/EventDetailPage/NormalProfile.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Box, HStack, Container } from '@chakra-ui/react';
+import { Box, Flex, Container } from '@chakra-ui/react';
 import { PRI_BACKGROUND } from 'constants/styles';
 
 import PropTypes from 'prop-types';
@@ -8,13 +8,13 @@ import parserHtml from 'utils/html';
 
 // If you want to use your own Selectors look up the Advancaed Story book examples
 const NormalProfile = ({ profile }) => (
-  <Container ps={0}>
-    <HStack w={{ '2xl': '73rem', xl: '57rem' }}>
+  <Container p={0} w="100%" maxWidth="inherit">
+    <Flex direction={{ sm: 'column', md: 'row' }} w="100%" height="25rem">
       <ProfileCard data={profile.organizer} avatar={profile.organizer.avatar} />
-      <Box bg={PRI_BACKGROUND} h="26rem" w="1000%" p={4}>
+      <Box bg={PRI_BACKGROUND} w="100%" p={4} mt={{ sm: '1rem', lg: 0 }}>
         {parserHtml(profile.organizer.bio)}
       </Box>
-    </HStack>
+    </Flex>
   </Container>
 );
 
