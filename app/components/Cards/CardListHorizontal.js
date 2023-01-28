e33a3c228be373b2d947c3d025189bc4862bda54
Bảo Đặng Kim <dangkimbao1999@gmail.com>
2023-01-29 02:52:08 +0700

    fix: change size of card in homepage

--- app/components/Cards/CardListHorizontal.js
+++ app/components/Cards/CardListHorizontal.js
@@ -11,7 +11,7 @@ const CardListHorizontal = ({
   quantity,
   width,
 }) => (
-  <Container maxW="100%" centerContent paddingInlineStart={0}>
+  <Container maxW="100%" paddingInlineStart={0}>
     <SimpleGrid maxW="100%" columns={columns} spacing={spacing}>
       {/* eslint-disable-next-line func-names */}
       {dataList.map(function(data, index) {
